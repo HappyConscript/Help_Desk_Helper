343a400927638a142df298dea47c1a7cb3ef4aab
Evan Brooks <37019669+HappyConscript@users.noreply.github.com>
2018-06-28 15:44:36 -0400

    Add files via upload

---
+++ Incident.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Author: Evan Brooks
+ * Organization: Town of Cary
+ * Date: 6/28/2018
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskHelper
+{
+    public class Incident
+    {
+        /*
+         * ______________________________________________________________________________________________________________________________________________________________
+         * Properties
+         * This section contains global variables and properties utilized to define this object
+         */
+
+        public string User { get; set; }
+        public string Phone { get; set; }
+        public DateTime Date { get; set; }
+        public Boolean Existing { get; set; }
+        public Boolean Urgent { get; set; }
+		public string Description { get; set; }
+
+        /* 
+         * ______________________________________________________________________________________________________________________________________________________________
+         * Constructor
+         * This section contains a function utilized in the instantiation of this object
+         */
+
+        public Incident(string user, string phone, DateTime date, Boolean existing, Boolean urgent, string description)
+        {
+            this.User = user;
+            this.Phone = phone;
+            this.Date = date;
+            this.Existing = existing;
+            this.Urgent = urgent;
+			this.Description = description;
+        }
+
+        /* 
+         * ______________________________________________________________________________________________________________________________________________________________
+         * Functions
+         * This section contains the functions which define the behaviors of this object
+         */
+
+        public override string ToString()
+        {
+			string incidentPrint = "User: " + User + " Phone: " + Phone + " Date: " + Date + " Existing: " + Existing + " Urgent: " + Urgent + " Description: " + Description; 
+            return incidentPrint;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Author: Evan Brooks
+ * Organization: Town of Cary
+ * Date: 6/28/2018
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelpDeskHelper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMain());
+        }
+    }
+}
